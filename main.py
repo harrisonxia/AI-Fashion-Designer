d9f39c0f2d99e8e5a0310f35fc545af762306fed
Daniel D Xiang <32584509+xiangdaniel@users.noreply.github.com>
2018-11-30 17:10:10 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,222 +1,85 @@
-import argparse
 import os
+import sys
 import random
-import time
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+import PIL
+from PIL import Image
+import models.mrcnn.coco as coco
+import models.mrcnn.utils as utils
+import models.mrcnn.model as modellib
+import models.mrcnn.visualize as visualize
+
 import torch
-import torch.nn.parallel
-import torch.backends.cudnn as cudnn
-import torch.utils.data
-import torchvision.utils as vutils
-from torch.autograd import Variable
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
-import torch.optim as optim
-
-import fashion_dcgan as dcgan
 
 
-def train():
-    # Define the parameters
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', required=True, help='data | folder | local')
-    parser.add_argument('--dataroot', required=True, help='path to dataset')
-    parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
-    parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
-    parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
-    parser.add_argument('--nc', type=int, default=3, help='input image channels')
-    parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
-    parser.add_argument('--ngf', type=int, default=64)
-    parser.add_argument('--ndf', type=int, default=64)
-    parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
-    parser.add_argument('--lrD', type=float, default=0.00005, help='learning rate for Critic, default=0.00005')
-    parser.add_argument('--lrG', type=float, default=0.00005, help='learning rate for Generator, default=0.00005')
-    parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
-    parser.add_argument('--cuda', action='store_true', help='enables cuda')
-    parser.add_argument('--ngpu', type=int, default=1, help='number of GPUs to use')
-    parser.add_argument('--netG', default='', help="path to netG (to continue training)")
-    parser.add_argument('--netD', default='', help="path to netD (to continue training)")
-    parser.add_argument('--clamp_lower', type=float, default=-0.01)
-    parser.add_argument('--clamp_upper', type=float, default=0.01)
-    parser.add_argument('--Diters', type=int, default=5, help='number of D iters per each G iter')
-    parser.add_argument('--noBN', action='store_true', help='use batchnorm or not (only for DCGAN)')
-    parser.add_argument('--n_extra_layers', type=int, default=0, help='Number of extra layers on gen and disc')
-    parser.add_argument('--experiment', default=None, help='Where to store samples and models')
-    parser.add_argument('--adam', action='store_true', help='Whether to use adam (default is rmsprop)')
-    opt = parser.parse_args()
-    print(opt)
+# Root directory of the project
+ROOT_DIR = os.getcwd()
 
-    if opt.experiment is None:
-        opt.experiment = 'samples'
-    os.system('mkdir {0}'.format(opt.experiment))
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
-    opt.manualSeed = random.randint(1, 10000)  # fix manual seed
-    print("Random Seed: ", opt.manualSeed)
-    random.seed(opt.manualSeed)
-    torch.manual_seed(opt.manualSeed)
+# Path to trained weights file
+# Download this file and place in the root of your
+# project (See README file for details)
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.pth")
 
-    cudnn.benchmark = True
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 
-    # Define the dataloader
-    if opt.dataset in ['data', 'folder', 'local']:
-        # folder dataset, './data'
-        transform = transforms.Compose([transforms.Resize(opt.imageSize),  # 224
-                                        transforms.CenterCrop(opt.imageSize),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                                        ])
-        dataset = datasets.ImageFolder(root=opt.dataroot, transform=transform)
-    assert dataset
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
-                                             shuffle=True, num_workers=int(opt.workers))
+class InferenceConfig(coco.CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    # GPU_COUNT = 0 for CPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
 
-    # set GPU
-    if torch.cuda.is_available() and not opt.cuda:
-        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
-    # device = torch.device("cuda:0" if opt.cuda else "cpu")
-    ngpu = int(opt.ngpu)
-    nz = int(opt.nz)
-    ngf = int(opt.ngf)
-    ndf = int(opt.ndf)
-    nc = int(opt.nc)  # 3
-    n_extra_layers = int(opt.n_extra_layers)
+config = InferenceConfig()
+config.display()
 
-    # custom weights initialization called on netG and netD
-    def weights_init(m):
-        classname = m.__class__.__name__
-        if classname.find('Conv') != -1:
-            m.weight.data.normal_(0.0, 0.02)
-        elif classname.find('BatchNorm') != -1:
-            m.weight.data.normal_(1.0, 0.02)
-            m.bias.data.fill_(0)
+# Create model object.
+model = modellib.MaskRCNN(model_dir=MODEL_DIR, config=config)
+if config.GPU_COUNT:
+    model = model.cuda()
 
-    # set the generator
-    if opt.noBN:
-        netG = dcgan.DCGAN_G_nobn(opt.imageSize, nz, nc, ngf, ngpu, n_extra_layers)
-    else:
-        netG = dcgan.DCGAN_G(opt.imageSize, nz, nc, ngf, ngpu, n_extra_layers)
+# Load weights trained on MS-COCO
+model.load_state_dict(torch.load(COCO_MODEL_PATH))
 
-    netG.apply(weights_init)
-    if opt.netG != '':  # load checkpoint if needed
-        netG.load_state_dict(torch.load(opt.netG))
-    print(netG)
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
 
-    # set the discriminator
-    netD = dcgan.DCGAN_D(opt.imageSize, nz, nc, ndf, ngpu, n_extra_layers)
-    netD.apply(weights_init)
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
 
-    if opt.netD != '':
-        netD.load_state_dict(torch.load(opt.netD))
-    print(netD)
+# Load a random image from the images folder
+#file_names = next(os.walk(IMAGE_DIR))[2]
+#image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
 
-    input = torch.FloatTensor(opt.batchSize, 3, opt.imageSize, opt.imageSize)
-    noise = torch.FloatTensor(opt.batchSize, nz, 1, 1)
-    fixed_noise = torch.FloatTensor(opt.batchSize, nz, 1, 1).normal_(0, 1)
-    one = torch.FloatTensor([1])
-    mone = one * -1
-
-    if opt.cuda:
-        netD.cuda()
-        netG.cuda()
-        input = input.cuda()
-        one, mone = one.cuda(), mone.cuda()
-        noise, fixed_noise = noise.cuda(), fixed_noise.cuda()
-
-    # setup optimizer
-    if opt.adam:
-        optimizerD = optim.Adam(netD.parameters(), lr=opt.lrD, betas=(opt.beta1, 0.999))
-        optimizerG = optim.Adam(netG.parameters(), lr=opt.lrG, betas=(opt.beta1, 0.999))
-    else:
-        optimizerD = optim.RMSprop(netD.parameters(), lr=opt.lrD)
-        optimizerG = optim.RMSprop(netG.parameters(), lr=opt.lrG)
-
-    # Do the training
-    gen_iterations = 0
-    for epoch in range(opt.niter):
-        data_iter = iter(dataloader)
-        i = 0
-        while i < len(dataloader):
-            # (1) Update D network
-            for p in netD.parameters():  # reset requires_grad
-                p.requires_grad = True  # they are set to False below in netG update
-
-            # train the discriminator Diters times
-            if gen_iterations < 25 or gen_iterations % 500 == 0:
-                Diters = 100
-            else:
-                Diters = opt.Diters
-            j = 0
-            while j < Diters and i < len(dataloader):
-                j += 1
-
-                # clamp parameters to a cube
-                for p in netD.parameters():
-                    p.data.clamp_(opt.clamp_lower, opt.clamp_upper)
-
-                data = data_iter.next()
-                i += 1
-
-                # train with real
-                real_cpu, _ = data
-                netD.zero_grad()
-                batch_size = real_cpu.size(0)
-
-                if opt.cuda:
-                    real_cpu = real_cpu.cuda()
-                input.resize_as_(real_cpu).copy_(real_cpu)
-                inputv = Variable(input)
-
-                errD_real = netD(inputv)
-                errD_real.backward(one)
-
-                # train with fake
-                noise.resize_(opt.batchSize, nz, 1, 1).normal_(0, 1)
-                noisev = Variable(noise, volatile=True)  # totally freeze netG
-                fake = Variable(netG(noisev).data)
-                inputv = fake
-                errD_fake = netD(inputv)
-                errD_fake.backward(mone)
-                errD = errD_real - errD_fake
-                optimizerD.step()
-
-            # (2) Update G network
-            for p in netD.parameters():
-                p.requires_grad = False  # to avoid computation
-            netG.zero_grad()
-            # in case our last batch was the tail batch of the dataloader,
-            # make sure we feed a full batch of noise
-            noise.resize_(opt.batchSize, nz, 1, 1).normal_(0, 1)
-            noisev = Variable(noise)
-            fake = netG(noisev)
-            errG = netD(fake)
-            errG.backward(one)
-            optimizerG.step()
-            gen_iterations += 1
-
-            print('[%d/%d][%d/%d][%d] Loss_D: %f Loss_G: %f Loss_D_real: %f Loss_D_fake %f'
-                  % (epoch, opt.niter, i, len(dataloader), gen_iterations,
-                     errD.data[0], errG.data[0], errD_real.data[0], errD_fake.data[0]))
-            if gen_iterations % 500 == 0:
-                real_cpu = real_cpu.mul(0.5).add(0.5)
-                vutils.save_image(real_cpu, '{0}/real_samples.png'.format(opt.experiment))
-                fake = netG(Variable(fixed_noise, volatile=True))
-                fake.data = fake.data.mul(0.5).add(0.5)
-                vutils.save_image(fake.data, '{0}/fake_samples_{1}.png'.format(opt.experiment, gen_iterations))
-
-        # do checkpointing
-        torch.save(netG.state_dict(), '{0}/netG_epoch_{1}.pth'.format(opt.experiment, epoch))
-        torch.save(netD.state_dict(), '{0}/netD_epoch_{1}.pth'.format(opt.experiment, epoch))
-
-    print('Finished Training')
-
-    # TODO: test
-
-    print("debug")
-    nb_cuda = torch.cuda.device_count()
-    print('We used ', nb_cuda, 'GPUs')
-
-
-if __name__ == '__main__':
-    tmps1 = time.time()
-    train()
-    tmps2 = time.time() - tmps1
-    print("Time of execution = %f seconds" % tmps2)
+i = 0
+for file_name in os.listdir(IMAGE_DIR):
+    image = skimage.io.imread(os.path.join(IMAGE_DIR, file_name))
+    if image is not None:
+        i += 1
+        results = model.detect([image])  # Run detection
+        # Visualize results
+        r = results[0]
+        visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
+        plt.show()
+        plt.savefig(os.path.join('./results_mrcnn', 'result_%d.png' % i))
